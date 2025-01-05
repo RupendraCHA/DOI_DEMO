f535521efe3f4ceb8ad356d1f00cd2f825e3173a
RupendraCHA <chandaluri210@gmail.com>
2025-01-06 01:17:09 +0530

    Adding input fields for Signup Page

--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -5,7 +5,34 @@ const SignUp = () => {
   return (
     <div className="signup-container">
       <div className="container signup-section">
-        <h1>Sign Up</h1>
+        <div className="signup-section-container">
+          <h1>Sign Up</h1>
+          <div>
+            <div>
+              <label id="firstname">Firstname</label>
+              <br />
+              <input type="text" htmlFor="firstname" placeholder="Firstname" />
+            </div>
+            <div>
+              <label id="lastname">Lastname</label>
+              <br />
+              <input type="text" htmlFor="lastname" placeholder="Lastname" />
+            </div>
+          </div>
+          <div>
+            <label id="mobile">Mobile Number</label>
+            <br />
+            <input type="text" htmlFor="mobile" placeholder="Mobile Number" />
+          </div>
+          <div>
+            <label id="password">Password</label>
+            <br />
+            <div>
+              <input type="password" placeholder="Password" />
+              <input type="text" placeholder="Confirm Password" />
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
