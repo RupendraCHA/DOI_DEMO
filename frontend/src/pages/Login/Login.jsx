3b715944a8e76402eb6f5ed432353cae7437a8b0
RupendraCHA <chandaluri210@gmail.com>
2025-01-06 10:32:16 +0530

    Adding input fields to Sign up and Login pages

--- frontend/src/pages/Login/Login.jsx
+++ frontend/src/pages/Login/Login.jsx
@@ -1,11 +1,53 @@
 import React from "react";
 import "./Login.css";
 
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 const Login = () => {
   return (
     <div className="login-container">
       <div className="container login-section">
-        <h1>Login</h1>
+        <form className="login-section-container">
+          <h1>Login</h1>
+
+          <div className="info-section">
+            <div>
+              <label id="email">Email</label>
+              <br />
+              <input
+                type="text"
+                htmlFor="email"
+                required
+                placeholder="Email*"
+              />
+            </div>
+          </div>
+          <div className="info-section">
+            <div>
+              <label id="password">Password</label>
+              <br />
+              <input
+                type="password"
+                htmlFor="password"
+                required
+                placeholder="Password*"
+              />
+            </div>
+          </div>
+
+          <div className="signup-button">
+            <button type="submit">Login</button>
+          </div>
+          <div className="new-here">
+            <p>
+              New here ?
+              <Link to="/signup" className="login-link">
+                <span>Sign Up</span>
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
--- frontend/src/pages/SignUp/SignUp.jsx
+++ frontend/src/pages/SignUp/SignUp.jsx
@@ -1,38 +1,97 @@
 import React from "react";
+import { BsFillInfoCircleFill } from "react-icons/bs";
+import { Link } from "react-router-dom";
+
 import "./SignUp.css";
 
 const SignUp = () => {
   return (
     <div className="signup-container">
       <div className="container signup-section">
-        <div className="signup-section-container">
+        <form className="signup-section-container">
           <h1>Sign Up</h1>
-          <div>
+          <div className="info-section">
             <div>
               <label id="firstname">Firstname</label>
               <br />
-              <input type="text" htmlFor="firstname" placeholder="Firstname" />
+              <input
+                type="text"
+                htmlFor="firstname"
+                required
+                placeholder="Firstname*"
+              />
             </div>
             <div>
               <label id="lastname">Lastname</label>
               <br />
-              <input type="text" htmlFor="lastname" placeholder="Lastname" />
+              <input
+                type="text"
+                htmlFor="lastname"
+                required
+                placeholder="Lastname*"
+              />
             </div>
           </div>
-          <div>
-            <label id="mobile">Mobile Number</label>
-            <br />
-            <input type="text" htmlFor="mobile" placeholder="Mobile Number" />
-          </div>
-          <div>
-            <label id="password">Password</label>
-            <br />
+          <div className="info-section">
             <div>
-              <input type="password" placeholder="Password" />
-              <input type="text" placeholder="Confirm Password" />
+              <label id="mobile">Mobile Number</label>
+              <br />
+              <input
+                type="text"
+                htmlFor="mobile"
+                required
+                placeholder="Mobile Number*"
+              />
+            </div>
+            <div>
+              <label id="email">Email</label>
+              <br />
+              <input
+                type="text"
+                htmlFor="email"
+                required
+                placeholder="Email*"
+              />
             </div>
           </div>
-        </div>
+          <div className="info-section">
+            <div>
+              <label id="password">Password</label>
+              <br />
+              <input
+                type="password"
+                htmlFor="password"
+                required
+                placeholder="Password*"
+              />
+            </div>
+            <div>
+              <label id="confirm">Confirm</label>
+              <br />
+              <input
+                type="password"
+                htmlFor="confirm"
+                required
+                placeholder="Confirm Password*"
+              />
+            </div>
+          </div>
+          <div className="pass-requirement">
+            <BsFillInfoCircleFill className="info-icon" />
+            <p>Password must be atleast 8 characters.</p>
+          </div>
+          <div className="signup-button">
+            <button type="submit">Sign Up</button>
+          </div>
+          <div className="have-account">
+            <p>
+              Already have an account ?
+              <Link to="/login" className="login-link">
+                <span>Login</span>
+              </Link>
+            </p>
+          </div>
+        </form>
       </div>
     </div>
   );
