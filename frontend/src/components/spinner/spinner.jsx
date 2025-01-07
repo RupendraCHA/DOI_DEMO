d4d0bca08d61738e6b67c9d379414a7c25de567a
RupendraCHA <chandaluri210@gmail.com>
2025-01-07 12:13:56 +0530

    Modifying the spinner component file importing modules

--- frontend/src/components/spinner/spinner.jsx
+++ frontend/src/components/spinner/spinner.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import "./Spinner.css"; // Import CSS for styling
+import "./spinner.css"; // Import CSS for styling
 
 // Spinner component
 const Spinner = ({
--- frontend/src/context/StoreContext.jsx
+++ frontend/src/context/StoreContext.jsx
@@ -4,6 +4,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   let url = "http://localhost:5000";
+  // let url = "https://doi-demo.onrender.com";
 
   const contextValue = {
     url,
